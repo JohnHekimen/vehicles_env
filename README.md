23cca5dc3b031dab40e27267a489f42cf2ee3826
JohnHekimen <johnhekemenport@gmail.com>
2026-07-10 23:35:06 +0100

    Último commit

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # vehicles_env
+
 Análise de veiculos.
 
 Esse app tem como objetivo a criação de histograma e também de gráfico de dispersão em relação ao arquivo vehicles_us.csv
+
+
+# Observação:
+Durante os testes encontrei um comportamento inesperado no Render. Quando adiciono um st.header() ou st.title(), a aplicação funciona normalmente em ambiente local (executando pelo Git Bash). No entanto, após o deploy no Render, o primeiro gráfico é exibido corretamente, mas ao tentar exibir o segundo gráfico, o servidor interrompe a execução da aplicação. Sem o cabeçalho, esse problema não ocorre. Como o código funciona corretamente em ambiente local, acredito que esse comportamento esteja relacionado ao ambiente de hospedagem do Render. Tentei várias formas se solucionar, mas não tive sucesso.
+
+
+Link da aplicação online no Render:
+
+https://vehicles-env-0kca.onrender.com
--- app.py
+++ app.py
@@ -8,15 +8,25 @@ car_data = pd.read_csv('vehicles_us.csv') # lendo os dados
 
 hist_button = st.checkbox('Criar histograma') # botão para criar histograma
 
-grafico = st.selectbox(
-     'Escolha o gráfico que deseja visualizar', 
-    ['Histograma', 'Gráfico de dispersão', 'Gráfico de barras']
-) # menu suspenso para escolher o gráfico
+if hist_button: # Se o botão for clicado
+    st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
 
-if grafico == 'Histograma':
-    fig = px.histogram(car_data, x="odometer")
+    fig = px.histogram(
+        car_data, 
+        x="odometer"
+    ) # criar um histograma
 
-else:
-    fig = px.scatter(car_data, x="odometer", y="price")
+    st.plotly_chart(fig, width="stretch") # exibindo o histograma
 
-st.plotly_chart(fig, width="stretch") # exibindo o gráfico
+
+scatter_button = st.checkbox('Criar gráfico de dispersão') # botão para criar gráfico de dispersão
+
+if scatter_button: # Se o botão for clicado
+    st.write('Criando um gráfico de dispersão para o conjunto de dados de anúncios de vendas de carros')
+
+    fig = px.scatter(
+        car_data,
+        x='odometer',
+        y='price'
+    )
+    st.plotly_chart(fig, width="stretch")
