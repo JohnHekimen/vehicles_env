5fa20c26f82d70bb9fd94c79b764c0431f1b7154
JohnHekimen <johnhekemenport@gmail.com>
2026-07-10 22:53:30 +0100

    Adicionando cabeçalho

--- app.py
+++ app.py
@@ -2,12 +2,11 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.header("Análise de Dados")
+st.header("Análise de Dados para anúncios de vendas de carros")
 
 car_data = pd.read_csv('vehicles_us.csv') # lendo os dados
 
-hist_button = st.button('Criar histograma') # botão para criar histograma
-scatter_button = st.button('Criar gráfico de dispersão') # botão para criar gráfico de dispersão
+hist_button = st.checkbox('Criar histograma') # botão para criar histograma
 
 if hist_button: # Se o botão for clicado
     st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
@@ -18,6 +17,9 @@ if hist_button: # Se o botão for clicado
     ) # criar um histograma
 
     st.plotly_chart(fig, width="stretch") # exibindo o histograma
+
+
+scatter_button = st.checkbox('Criar gráfico de dispersão') # botão para criar gráfico de dispersão
 
 if scatter_button: # Se o botão for clicado
     st.write('Criando um gráfico de dispersão para o conjunto de dados de anúncios de vendas de carros')
